2a07347b772cce579f9dbdbf398f6d94ac911554
lmaksin <lmaksin@umich.edu>
2021-12-24 23:49:59 -0500

    changed about me

--- src/App.js
+++ src/App.js
@@ -32,12 +32,10 @@ function App() {
         <h3>a little more about me</h3>
         <p class="mt-3">I am a student at the University of Michigan majoring in Computer Science and Business Administration, 
         and minoring in Mathematics. I've always had an intense passion for creating, and have built anything from rockets 
-        to physics simulations to WebGL websites. I also have strong interests in startup, product, growth, software development, 
-        and innovation in the fields of aerospace, edtech, community-building, and breaking down barriers to entry in general. 
+        to physics simulations to WebGL websites. I also have strong interests in startup, product, growth, software, 
+        and innovation in the fields of aerospace, crpyto, edtech, community-building, and want to break down barriers. 
         I strive to create as much as possible, and in my free time, I enjoy reading, exercising, playing card games and chess, 
-        and spending time with my five younger siblings. Contact me at 
-        <a class="email-link" href="mailto: lmaksin@umich.edu">lmaksin@umich.edu</a>, 
-        I love making new friends!</p>
+        and spending time with my five younger siblings. Contact me at <a class="email-link" href="mailto: lmaksin@umich.edu">lmaksin@umich.edu</a>!</p>
       </div>
     )
   }
